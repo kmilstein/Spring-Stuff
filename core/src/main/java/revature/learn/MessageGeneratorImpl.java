dcdc28617bf666a396c032972a60686d05beff59
Ksenia <kxmilste@oaklandcc.edu>
2020-12-21 12:39:08 -0500

    MessageGeneratorImpl's message methods were refactored to add game's message logic.

--- core/src/main/java/revature/learn/MessageGeneratorImpl.java
+++ core/src/main/java/revature/learn/MessageGeneratorImpl.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.annotation.PostConstruct;
 
-public class MessageGeneratorImpl implements MessageGenerator{
+public class MessageGeneratorImpl implements MessageGenerator {
 
     //== constants ==//
     private static final Logger log = LoggerFactory.getLogger(GameImpl.class);
@@ -26,11 +26,31 @@ public class MessageGeneratorImpl implements MessageGenerator{
     //== public methods ==//
     @Override
     public String getMainMessage() {
-        return "Main message string";
+        return "Number is between" +
+                game.getSmallest() +
+                " and " +
+                game.getBiggest() +
+                ". Can you guess it?";
     }
 
     @Override
     public String getResultMessage() {
-        return "Result message string";
+        if (game.isGameLost()) {
+            return "You guessed it! The number was " + game.getNumber();
+        } else if (game.isGameLost()) {
+            return "You lost. The number was " + game.getNumber();
+        } else if (!game.isValidNumberRange()) {
+            return "Invalid number range!";
+        } else if (game.getRemainingGuesses() == guessCount) {
+            return "What is your first guess?";
+        } else {
+            String direction = "Lower";
+
+            if (game.getGuess() < game.getNumber()) {
+                direction = "Higher";
+            }
+
+            return direction + "! You have " + game.getRemainingGuesses() + "guesses left";
+        }
     }
 }
