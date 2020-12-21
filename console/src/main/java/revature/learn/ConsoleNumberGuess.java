f747e739dceb241cff47a6eb4ed30eb7f09b72b9
Ksenia <kxmilste@oaklandcc.edu>
2020-12-21 12:59:20 -0500

    In ConsoleNumberGuess removed ApplicationListener and used annotation instead. The name of onApplicationEvent() method was changed to start()

--- console/src/main/java/revature/learn/ConsoleNumberGuess.java
+++ console/src/main/java/revature/learn/ConsoleNumberGuess.java
@@ -4,16 +4,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ConsoleNumberGuess implements ApplicationListener<ContextRefreshedEvent> {
+public class ConsoleNumberGuess {
 
     //== constants ==//
     private static final Logger log = LoggerFactory.getLogger(ConsoleNumberGuess.class);
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        log.info("Container ready for use.");
+    @EventListener
+    public void start(ContextRefreshedEvent contextRefreshedEvent) {
+        log.info("start() --> Container ready for use.");
     }
 }
