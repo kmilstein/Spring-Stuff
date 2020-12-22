b2864c7ffcd8de78a72f7ae36748b085754edf90
Ksenia <kxmilste@oaklandcc.edu>
2020-12-21 22:41:05 -0500

    Refactor: Lombok refactor continued.
    Getter anotation was added to top of the GameImpl class and NumberGenerator field's AccessLevel was set to none. Getter anonations were added tk maxNumber and minNumber in NumberGeneratorImpl.

--- core/src/main/java/revature/learn/GameImpl.java
+++ core/src/main/java/revature/learn/GameImpl.java
@@ -1,8 +1,9 @@
 package revature.learn;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,18 +11,24 @@ import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 @Slf4j
+@Getter
 @Component
 public class GameImpl implements Game {
 
     //== fields == //
+    @Getter(AccessLevel.NONE)
     private final NumberGenerator numberGenerator;
+
     private final int guessCount;
+
     private int number;
-    private int guess;
     private int smallest;
     private int biggest;
     private int remainingGuesses;
     private boolean validNumberRange = true;
+
+    @Setter
+    private int guess;
 
     // == constructors == //
     @Autowired
@@ -49,39 +56,6 @@ public class GameImpl implements Game {
 
     //== public methods == //
     @Override
-    public int getNumber() {
-        return number;
-    }
-
-    @Override
-    public int getGuess() {
-        return guess;
-    }
-
-    @Override
-    public void setGuess(int guess) {
-        this.guess = guess;
-    }
-
-    @Override
-    public int getSmallest() {
-        return smallest;
-    }
-
-    @Override
-    public int getBiggest() {
-        return biggest;
-    }
-
-    @Override
-    public int getRemainingGuesses() {
-        return remainingGuesses;
-    }
-
-    @Override
-    public int getGuessCount() { return guessCount; }
-
-    @Override
     public void check() {
         checkValidNumberRange();
 
@@ -94,11 +68,6 @@ public class GameImpl implements Game {
         }
 
         remainingGuesses--;
-    }
-
-    @Override
-    public boolean isValidNumberRange() {
-        return validNumberRange;
     }
 
     @Override
